3ecd68b8faf762dc41e506e41b56fbf2d408e5d0
ykhost <andrew.heringer1337@gmail.com>
2022-06-02 16:31:06 -0300

    refactor(Home page): :art: Componetization of the search type selection button

--- src/components/Home/Options/Button.tsx
+++ src/components/Home/Options/Button.tsx
@@ -1,23 +1,34 @@
 import { Button as ButtonChakra, ButtonProps as ButtonChakraProps } from "@chakra-ui/react";
 import { typeSearch } from "../../../Interfaces/searchType";
+import { ActiveButton } from "../../ActiveButton";
 
 interface ButtonProps extends ButtonChakraProps{
-  handleSelectSearch: (typeSearch: typeSearch) => void
-
+  handleSelectSearch: (typeSearch: typeSearch) => void;
+  children: string;
+  isActive: boolean;
+  vehicleType: typeSearch;
 }
 
-export function Button ({handleSelectSearch}: ButtonProps) {
+export function Button ({
+  handleSelectSearch,
+  children,
+  isActive,
+  vehicleType
+  }: ButtonProps) {
   return (
-    <ButtonChakra
-      fontSize={["1.1rem","1.5rem"]}
-      fontWeight="medium"
-      _hover={{ bg: "inherit", color: "black" }}
-      _focus={{
+    <ActiveButton isActive={isActive}>
+      <ButtonChakra
+        fontSize={["1.1rem","1.5rem"]}
+        fontWeight="medium"
+        _hover={{ bg: "inherit", color: "black" }}
+        _focus={{
 
-      }}
-      onClick={() => handleSelectSearch("automobiles")}
-    >
-      Automoveis
-    </ButtonChakra>
+        }}
+        onClick={() => handleSelectSearch(vehicleType)}
+      >
+        { children }
+      </ButtonChakra>
+    </ActiveButton>
+
   )
 }
